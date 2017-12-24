4545bc2ad8ca17af4990f0762bbca920c270873d
Francesco Oliva <olibartfast@gmail.com>
2017-12-24 08:59:39 +0100

    working con C# porting

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Python and C++ optical flow tracking
+# Python, C++ and C# optical flow tracking
 Using Optical Flow for Detection Motion object.
 
 
@@ -7,9 +7,11 @@ Original code from the forked repo and  https://github.com/opencv/opencv/blob/2.
 1. using video path : python optical_flow.py [video source]  
 2. using camera : python optical_flow.py   
 
-Few little changing to the original python script to make it run with opencv 3.2.0 and ported also to C++  
+Few little changing to the original python script to make it run with opencv 3.2.0 and ported also to C++  and C#
 
 A C++ example using the optical flow algorithm described by Gunnar Farneback can be found at https://github.com/opencv/opencv/blob/master/samples/cpp/fback.cpp
 
 the C++ flow -> hsv -> bgr function:  
-https://stackoverflow.com/questions/7693561/opencv-displaying-a-2-channel-image-optical-flow
+https://stackoverflow.com/questions/7693561/opencv-displaying-a-2-channel-image-optical-flow  
+  
+C# code developed with Visual Studio 2015 and opencvsharp(3.2.0) library
---
+++ optical_flow.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenCvSharp;
+
+namespace OpticalFlowTracking
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Mat flow, cflow, gray, prevgray, img_bgr, img_hsv;
+            Point[][] contours;
+            HierarchyIndex[] hierarchy;
+            prevgray = new Mat();
+
+            VideoCapture cap = new VideoCapture();
+            cap.Open(0);
+            int sleepTime = (int)Math.Round(1000 / cap.Fps);
+
+            using (Window window = new Window("capture"))
+            using (Mat frame = new Mat()) // Frame image buffer
+            {
+                while (true)
+                {
+                    cap.Read(frame); 
+                    if (frame.Empty())
+                        break;
+                    gray = new Mat();
+                    flow = new Mat();
+                    cflow = new Mat();
+                    img_bgr = new Mat();
+                    Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+                    if (prevgray.Empty())
+                        prevgray = gray;
+                    else {
+                        Cv2.CalcOpticalFlowFarneback(prevgray, gray, flow, 0.5, 5, 16, 3, 5, 1.2, OpticalFlowFlags.FarnebackGaussian);
+                        Cv2.CvtColor(prevgray, cflow, ColorConversionCodes.GRAY2BGR);
+                        drawOptFlowMap(ref flow, ref cflow, 1.5, 16, new Scalar(0, 0, 255));
+                        drawHsv(flow, out img_bgr);
+                        Mat gray_bgr = new Mat();
+                        gray_bgr = Mat.Zeros(frame.Rows, frame.Cols, MatType.CV_8UC1);
+                        Cv2.CvtColor(img_bgr, gray_bgr, ColorConversionCodes.BGR2GRAY);
+                        Cv2.Normalize(gray_bgr, gray_bgr, 0, 255, NormTypes.MinMax, MatType.CV_8UC1);
+                        Cv2.Blur(gray_bgr, gray_bgr, new Size(3, 3));
+
+                        // Detect edges using Threshold
+                        Mat img_thresh = new Mat();
+                        //window.Image = gray_bgr;
+                        Char c = (Char)Cv2.WaitKey(1);
+                        if (c == 27) break;
+                        Swap<Mat>(ref gray, ref prevgray);
+                    }
+                }
+            }
+        }
+
+
+        private static void drawOptFlowMap(ref Mat flow, ref Mat cflowmap, double scale, int step, Scalar color){
+            for (int y = 0; y < cflowmap.Rows; y += step)
+                for (int x = 0; x < cflowmap.Cols; x += step) {
+                    Point2f fxy = flow.At<Point2f>(y, x) * scale;
+                    Cv2.Line(cflowmap, new Point(x, y), new Point(Math.Round(x + fxy.X), Math.Round(y + fxy.Y)), color);
+                    Cv2.Circle(cflowmap, new Point(x, y), 2, color, -1);
+                }
+        }
+
+        private static void drawHsv(Mat flow, out Mat bgr)
+        {
+            Mat[] xy = new Mat[2];
+            Cv2.Split(flow, out xy);
+            Mat magnitude = new Mat();
+            Mat angle = new Mat();
+            Cv2.CartToPolar(xy[0], xy[1], magnitude, angle, true);
+            //translate magnitude to range [0;1]
+            double mag_max, mag_min;
+            Cv2.MinMaxLoc(magnitude, out mag_min, out mag_max);
+            magnitude.ConvertTo(
+                magnitude,    // output matrix
+                -1,           // type of the ouput matrix, if negative same type as input matrix
+                1.0 / mag_max // scaling factor
+             );
+            //build hsv image
+            Mat[] _hsv = new Mat[3];
+            Mat hsv = new Mat();
+            bgr = new Mat();
+            _hsv[0] = angle;
+            _hsv[1] = magnitude;
+            _hsv[2] = Mat.Ones(angle.Size(), MatType.CV_32F);
+            Cv2.Merge(_hsv, hsv);
+            Cv2.CvtColor(hsv, bgr, ColorConversionCodes.HSV2BGR);
+            Cv2.ImShow("hsv2bgr", bgr);
+        }
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+    }
+}
